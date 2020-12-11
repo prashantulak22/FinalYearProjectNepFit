7cdaaaaa8ce538d223288b93485e1d68f951fa6d
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-11 20:16:17 +0545

    created progress tracker and bodymetrics table.

---
+++ src/NEPFIT/FinalYearProject_NepFit/BodyMetrics.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[BodyMetrics]
+(
+	Height decimal(3,2) NOT NULL,
+	BodyMass decimal(5,2) NOT NULL,
+	ChestSize decimal(4,2) NOT NULL,
+	ArmSize decimal(4,2) NOT NULL,
+	ForearmSize decimal(4,2) NOT NULL,
+	WristSize decimal(4,2) NOT NULL,
+	NeckSize decimal(4,2) NOT NULL,
+	UpperAbs decimal(4,2) NOT NULL,
+	LowerAbs decimal(4,2) NOT NULL,
+	HipSize decimal(4,2) NOT NULL,
+	WaistSize decimal(4,2) NOT NULL,
+	ThighSize decimal(4,2) NOT NULL,
+	CalveSize decimal(4,2) NOT NULL,
+	[UserId] UNIQUEIDENTIFIER NOT NULL,
+	CONSTRAINT [FK_UserIdBodyMetrics] FOREIGN KEY ([UserId]) REFERENCES [NepFitUser]([UserId])
+)
---
+++ src/NEPFIT/FinalYearProject_NepFit/ProgressTracker.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[ProgressTracker]
+(
+	NewHeight decimal(3,2) NOT NULL,
+	NewBodyMass decimal(5,2) NOT NULL,
+	NewChestSize decimal(4,2) NOT NULL,
+	NewArmSize decimal(4,2) NOT NULL,
+	NewForearmSize decimal(4,2) NOT NULL,
+	NewWristSize decimal(4,2) NOT NULL,
+	NewNeckSize decimal(4,2) NOT NULL,
+	NewUpperAbs decimal(4,2) NOT NULL,
+	NewLowerAbs decimal(4,2) NOT NULL,
+	NewHipSize decimal(4,2) NOT NULL,
+	NewWaistSize decimal(4,2) NOT NULL,
+	NewThighSize decimal(4,2) NOT NULL,
+	NewCalveSize decimal(4,2) NOT NULL,
+)
