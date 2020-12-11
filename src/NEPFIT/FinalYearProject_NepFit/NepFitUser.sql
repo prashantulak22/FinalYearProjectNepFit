69147d3b6cd7e889bf6257707f700f603f6abb35
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-11 20:04:48 +0545

    created NepFitUser table

---
+++ src/NEPFIT/FinalYearProject_NepFit/NepFitUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[NepFitUser]
+(
+	[UserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [DateOfBirth] INT NOT NULL, 
+    [Gender] CHAR(1) NOT NULL, 
+    [MobileNumber] BIGINT NOT NULL
+)
