c2f8e184433a64c54257ffb31ef52eb378b758f2
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-11 19:55:13 +0545

    Added Nutrition Table.

---
+++ src/NEPFIT/FinalYearProject_NepFit/Nutrition.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Nutrition]
+(
+	NutritionId UNIQUEIDENTIFIER PRIMARY KEY,
+	WeightLossNutrition VARCHAR(MAX) NOT NULL,
+	WeightGainNutrition VARCHAR(MAX) NOT NULL,
+	FitNutrition VARCHAR(MAX) NOT NULL,
+)
