018b3793f24e450b7698d87edb981456dac2efec
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-13 10:37:28 +0545

    Adeed Model, Controller and edited some area.

---
+++ src/NEPFIT/NepFit.Api/Controllers/BodyMetricsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NepFit.Api.Controllers
+{
+    [ApiController]
+    public class BodyMetricsController : ControllerBase
+    {
+        [HttpPost]
+        public void CollectBodyMetrics()
+        {
+
+        }
+    }
+}
---
+++ src/NEPFIT/NepFit.Repository/Model/BodyMetrics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NepFit.Repository
+{
+    public class BodyMetrics
+    {
+        public decimal Height{ get; set; }
+
+        public decimal BodyMass { get; set; }
+
+        public decimal ChestSize { get; set; }
+
+        public decimal ArmSize { get; set; }
+
+        public decimal ForearmSize { get; set; }
+
+        public decimal WristSize { get; set; }
+
+        public decimal NeckSize { get; set; }
+        public decimal UpperAbs { get; set; }
+
+        public decimal LowerAbs { get; set; }
+
+        public decimal HipSize { get; set; }
+
+        public decimal WaistSize { get; set; }
+
+        public decimal ThighSize { get; set; }
+
+        public decimal CalveSize { get; set; }
+    }
+}
