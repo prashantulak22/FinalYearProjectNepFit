88b7f6350fc8ec1d30a38f8ebcf713a93a390dea
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-25 22:19:54 +0545

    Added NepFitUser and Nutrition.cs file

---
+++ src/NEPFIT/NepFit.Repository/Entity/Nutrition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NepFit.Repository.Entity
+{
+    class Nutrition
+    {
+        public Guid NutritionId { get; set; }
+
+    }
+}
