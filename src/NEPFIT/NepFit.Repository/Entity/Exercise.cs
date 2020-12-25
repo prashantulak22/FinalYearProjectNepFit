3ee030ef36f2fa92782e5e0297993d75e2534aa0
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2020-12-25 21:45:34 +0545

    added contents to exercise and exercisepackage .cs files.

--- src/NEPFIT/NepFit.Repository/Entity/Exercise.cs
+++ src/NEPFIT/NepFit.Repository/Entity/Exercise.cs
@@ -6,6 +6,19 @@ namespace NepFit.Repository.Entity
 {
     public class Exercise
     {
+        public Guid ExerciseId { get; set; }
+
+        public String ChestExercise { get; set; }
+
+        public String BackExercise { get; set; }
+        public String ShoulderExercise { get; set; }
+        public String BicepExercise { get; set; }
+        public String TricepExercise { get; set; }
+        public String LegExercise { get; set; }
+
+
+
+
 
     }
 }
---
+++ src/NEPFIT/NepFit.Repository/Entity/ExercisePackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NepFit.Repository.Entity
+{
+    class ExercisePackage
+    {
+        public Guid ExercisePackageId { get; set; }
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+
+    }
+}
