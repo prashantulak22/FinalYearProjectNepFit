08c72c05cb9997f2516b503bef20aba6c53969b3
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-02-20 10:15:13 -0600

    update the name ofthe db to PascalCase

--- src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
+++ src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[User_Exercise_Nutrition]
+﻿CREATE TABLE [dbo].[UserExerciseNutrition]
 (
 	[UserId] UNIQUEIDENTIFIER NOT NULL,
 	CONSTRAINT [FK_UserId]FOREIGN KEY ([UserId]) REFERENCES [NepFitUser]([UserId]),
