73a1b0b5fe648c932ea0f1c25bd3dba80de0b02d
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-02-27 07:47:04 +0545

    Added the repository section and its interface

---
+++ src/NEPFIT/NepFit.Repository/Repository/ExerciseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository.Interface;
+
+namespace NepFit.Repository.Repository
+{
+    public class ExerciseRepository : IExerciseRepository
+    {
+        private readonly ISqlServerConnectionProvider _sqlServerConnectionProvider;
+
+        public ExerciseRepository(ISqlServerConnectionProvider sqlServerConnectionProvider)
+        {
+            _sqlServerConnectionProvider = sqlServerConnectionProvider;
+        }
+
+        
+        public string Add(Exercise input)
+        {
+            var conn = _sqlServerConnectionProvider.GetDbConnection();
+            return conn.Execute()
+
+        }
+    }
+
+    }
+
---
+++ src/NEPFIT/NepFit.Repository/Repository/Interface/IExerciseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NepFit.Repository.Entity;
+
+namespace NepFit.Repository.Repository.Interface
+{
+    public interface IExerciseRepository
+    {
+        String Add(Exercise input);
+    }
+}
