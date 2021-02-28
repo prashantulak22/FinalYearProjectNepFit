be28b26064cb0815e35cbe428d38d1bb2f107e00
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-02-28 22:14:32 +0545

    Added the Dto part of NutritionRoutine

---
+++ src/NEPFIT/NepFit.Repository/Dto/NutritionCreateDto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NepFit.Repository.Dto
+{
+    class NutritionCreateDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealOne")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealOne { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealTwo")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealTwo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealThree")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealThree { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealFour")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealFour { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealFive")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealFive { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealSix")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealSix { get; set; }
+    }
+}
---
+++ src/NEPFIT/NepFit.Repository/Dto/NutritionResultDto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NepFit.Repository.Dto
+{
+    class NutritionResultDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("NutritionId")]
+        [DataMember]
+
+        public System.Guid NutritionId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealOne")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealOne { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealTwo")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealTwo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealThree")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealThree { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealFour")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealFour { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealFive")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealFive { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("MealSix")]
+        [DataMember]
+        [Required]
+        [StringLength(2147483647)]
+        public String MealSix { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("CreatedBy")]
+        [DataMember]
+        public String CreatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("UpdatedBy")]
+        [DataMember]
+
+        public String UpdatedBy { get; set; }
+    }
+}
