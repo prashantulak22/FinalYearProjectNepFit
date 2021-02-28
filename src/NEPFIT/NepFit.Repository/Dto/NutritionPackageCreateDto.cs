a44b4227779b3783c901e5c2db031afacab675be
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-02-28 22:27:48 +0545

    Created Dto files for NutritionPackage

---
+++ src/NEPFIT/NepFit.Repository/Dto/NutritionPackageCreateDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NepFit.Repository.Dto
+{
+    public class NutritionPackageCreateDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Name")]
+        [DataMember]
+        [Required]
+        [StringLength(5000)]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Description")]
+        [DataMember]
+        [Required]
+        [StringLength(5000)]
+        public String Description { get; set; }
+    }
+}
