932c99ecc1afee26a0b4e1041a3ab4ec7c74152e
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-01 20:51:20 +0545

    added contents in the interface layers of exercise and exercisepackage file

---
+++ src/NEPFIT/NepFit.BL/Interface/IExercise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NepFit.Repository.Dto;
+
+namespace NepFit.BL.Interface
+{
+    public interface IExercise
+    {
+        int AddExercise(ExerciseCreateDto inputDto);
+           
+    }
+}
