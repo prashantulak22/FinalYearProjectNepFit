fe235ce9f847ac4717fb997077198ea7b1b4d68d
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-04 21:03:54 +0545

    Added nitrition package service section

--- src/NEPFIT/NepFit.BL/NutritionPackageService.cs
+++ src/NEPFIT/NepFit.BL/NutritionPackageService.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository.Interface;
 
 namespace NepFit.BL
 {
-    class NutritionPackageService
+    public class NutritionPackageService : INutritionPackageService
     {
+        private readonly INutritionPackageRepository _nutritionPackageRepository;
+        private readonly IMapper _mapper;
+
+        public NutritionPackageService(INutritionPackageRepository nutritionPackageRepository, IMapper mapper)
+        {
+            _nutritionPackageRepository = nutritionPackageRepository;
+            _mapper = mapper;
+        }
+
+        public int AddNutritionPackage(NutritionPackageCreateDto inputDto)
+        {
+            return _nutritionPackageRepository.Add(_mapper.Map<NutritionPackage>(inputDto));
+        }
     }
 }
