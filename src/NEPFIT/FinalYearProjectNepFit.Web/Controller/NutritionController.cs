d465a0f8908789b01dea166b4cac3454bf8adb11
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-04 22:15:42 +0545

    added content to nutrition controller

--- src/NEPFIT/FinalYearProjectNepFit.Web/Controller/NutritionController.cs
+++ src/NEPFIT/FinalYearProjectNepFit.Web/Controller/NutritionController.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
 
 namespace FinalYearProjectNepFit.Web.Controller
 {
-    public class NutritionController
+    public class NutritionController : ControllerBase
     {
+        private readonly INutritionService _nutritionService;
+
+        public NutritionController(INutritionService nutritionService)
+        {
+            _nutritionService = nutritionService;
+        }
+
+        // [Route("api/exercise/package")]
+        // [HttpPost]
     }
 }
