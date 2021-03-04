73a32d9f40e4ec9160a7e0a54bd1e1b2e1509fa1
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-04 20:49:41 +0545

    Completed the ecerciseRoutine Sevice

--- src/NEPFIT/NepFit.BL/ExerciseRoutineService.cs
+++ src/NEPFIT/NepFit.BL/ExerciseRoutineService.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository.Interface;
 
 namespace NepFit.BL
 {
-    class ExerciseRoutineService
+    public class ExerciseRoutineService : IExerciseRoutineService
     {
+        private readonly IExerciseRoutineRepository _exerciseRoutineRepository;
+        private readonly IMapper _mapper;
+        public ExerciseRoutineService(IExerciseRoutineRepository exerciseRoutineRepository, IMapper mapper)
+        {
+            _exerciseRoutineRepository = exerciseRoutineRepository;
+            _mapper = mapper;
+
+        }
+
+        public int AddExerciseRoutine(ExerciseRoutineCreateDto inputDto)
+        {
+            return _exerciseRoutineRepository.Add(_mapper.Map<ExerciseRoutine>(inputDto));
+        }
     }
 }
