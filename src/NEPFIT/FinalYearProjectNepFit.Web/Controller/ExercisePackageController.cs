5686aa314fe2580a13c404ac8ca366e0b8c59a7f
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-04 22:15:04 +0545

    Added content to exercisepackage controller

--- src/NEPFIT/FinalYearProjectNepFit.Web/Controller/ExercisePackageController.cs
+++ src/NEPFIT/FinalYearProjectNepFit.Web/Controller/ExercisePackageController.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
 
 namespace FinalYearProjectNepFit.Web.Controller
 {
-    public class ExercisePackageController
+    [ApiController]
+    [Authorize]
+
+    public class ExercisePackageController : ControllerBase
     {
+        private readonly IExercisePackageService _exercisePackageService;
+
+        public ExercisePackageController(IExercisePackageService exercisePackageService)
+        {
+            _exercisePackageService = exercisePackageService;
+        }
+
+       // [Route("api/exercise/package")]
+       // [HttpPost]
     }
 }
