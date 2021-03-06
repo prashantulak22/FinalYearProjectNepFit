d930fb34bc0c4ddeb9f795cdcce952466add669e
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-05 22:06:05 -0600

    added service layer for excersise type

---
+++ src/NEPFIT/NepFit.BL/ExerciseTypeService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository;
+
+namespace NepFit.BL
+{
+    public class ExerciseTypeService : IExerciseTypeService
+    {
+        private readonly ExerciseTypeRepository _exerciseTypeRepository;
+        private readonly IMapper _mapper;
+
+        public ExerciseTypeService(ExerciseTypeRepository exerciseTypeRepository, IMapper mapper)
+        {
+            _exerciseTypeRepository = exerciseTypeRepository;
+            _mapper = mapper;
+        }
+
+        public int Add(ExerciseTypeCreateDto inputDto)
+        {
+            return _exerciseTypeRepository.Add(_mapper.Map<ExerciseType>(inputDto));
+        }
+
+        public bool Update(ExerciseTypeUpdateDto input)
+        {
+            _exerciseTypeRepository.Update(_mapper.Map<ExerciseType>(input));
+            return true;
+        }
+
+        public IEnumerable<ExerciseTypeResultDto> GetAll()
+        {
+            return
+                _mapper.Map<IEnumerable<ExerciseTypeResultDto>>(
+                _exerciseTypeRepository.GetAll());
+        }
+
+        public bool Delete(Guid id)
+        {
+            return _exerciseTypeRepository.Delete(id);
+        }
+    }
+}
---
+++ src/NEPFIT/NepFit.BL/Interface/IExerciseTypeService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using NepFit.Repository.Dto;
+
+namespace NepFit.BL.Interface
+{
+    public interface IExerciseTypeService
+    {
+        int Add(ExerciseTypeCreateDto input);
+        bool Update(ExerciseTypeUpdateDto input);
+        IEnumerable<ExerciseTypeResultDto> GetAll();
+        bool Delete(Guid id);
+    }
+}
