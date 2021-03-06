05856b9e9c3a70e66b04ebbbffd369c5e19f40dc
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-05 21:54:44 -0600

    update repository

--- src/NEPFIT/NepFit.Repository/Repository/ExerciseTypeRepository.cs
+++ src/NEPFIT/NepFit.Repository/Repository/ExerciseTypeRepository.cs
@@ -1,7 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Dapper;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository.Interface;
+
 namespace NepFit.Repository.Repository
 {
-    public class ExerciseTypeRepository
+    public class ExerciseTypeRepository : IExerciseTypeRepository
     {
+        private readonly ISqlServerConnectionProvider _sqlServerConnectionProvider;
 
-   }
+        public ExerciseTypeRepository(ISqlServerConnectionProvider sqlServerConnectionProvider)
+        {
+            _sqlServerConnectionProvider = sqlServerConnectionProvider;
+        }
+
+        public int Add(ExerciseType input)
+        {
+            var conn = _sqlServerConnectionProvider.GetDbConnection();
+            return conn.Execute("INSERT INTO ExerciseType ([ExerciseTypeId] ,[Name] ,[Description] " +
+                                ",[Active] ,[UpdatedBy] ,[CreatedBy] ,[DateUpdated] ,[DateCreated] )" +
+                                "	VALUES" +
+                                "	(@ExerciseTypeId ,@Name ,@Description " +
+                                ",@Active ,@UpdatedBy ,@CreatedBy ,@DateUpdated ,@DateCreated )", input);
+        }
+
+        public ExerciseType Update(ExerciseType input)
+        {
+            var conn = _sqlServerConnectionProvider.GetDbConnection();
+            conn.Execute("	UPDATE ExerciseType SET 	" +
+                                "	[ExerciseTypeId] = @ExerciseTypeId ,		" +
+                                "[Name] = @Name ,		[Description] = @Description ,	" +
+                                "	[Active] = @Active ,		[UpdatedBy] = @UpdatedBy ,	" +
+                                "	[CreatedBy] = @CreatedBy ,		[DateUpdated] = @DateUpdated ,		" +
+                                "[DateCreated] = @DateCreated 	WHERE [ExerciseTypeId]=@ExerciseTypeId", input);
+            return input;
+        }
+
+        public IEnumerable<ExerciseType> GetAll()
+        {
+            var conn = _sqlServerConnectionProvider.GetDbConnection();
+            return conn.Query<ExerciseType>(
+                "Select * From [dbo].[ExerciseType] WHERE Active = 1 order by DateCreated");
+        }
+
+        public bool Delete(Guid id)
+        {
+            var conn = _sqlServerConnectionProvider.GetDbConnection();
+            conn.Execute(" UPDATE ExerciseType SET 	" +
+                         "	[Active] = 0 ,		[UpdatedBy] = @UpdatedBy ,	" +
+                         " [DateUpdated] = @DateUpdated ,		" +
+                         " WHERE [ExerciseTypeId]=@ExerciseTypeId", new
+            {
+                ExerciseTypeId = id
+                         });
+            return true;
+        }
+    }
 }
--- src/NEPFIT/NepFit.Repository/Repository/Interface/IExerciseTypeRepository.cs
+++ src/NEPFIT/NepFit.Repository/Repository/Interface/IExerciseTypeRepository.cs
@@ -1,9 +1,14 @@
-using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using NepFit.Repository.Entity;
 
 namespace NepFit.Repository.Repository.Interface
 {
     public interface IExerciseTypeRepository
     {
-        
+        int Add(ExerciseType input);
+        ExerciseType Update(ExerciseType input);
+        IEnumerable<ExerciseType> GetAll();
+        bool Delete(Guid id);
     }
 }
