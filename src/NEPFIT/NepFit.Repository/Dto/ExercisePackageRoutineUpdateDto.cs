0f78cbf954757d8d21ab982b452ed691327e829c
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-06 19:36:56 +0545

    Added ExercisePackageRoutine essential file

---
+++ src/NEPFIT/NepFit.Repository/Dto/ExercisePackageRoutineUpdateDto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+
+namespace NepFit.Repository.Dto
+{
+    public class ExercisePackageRoutineUpdateDto
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("ExercisePackageRoutineId")]
+        [DataMember]
+
+        public System.Guid ExercisePackageRoutineId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("ExercisePackageId")]
+        [DataMember]
+        [Required]
+
+        public System.Guid ExercisePackageId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("ExerciseRoutineId")]
+        [DataMember]
+        [Required]
+
+        public System.Guid ExerciseRoutineId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Active")]
+        [DataMember]
+
+        public Boolean? Active { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("UpdatedBy")]
+        [DataMember]
+
+        public System.Guid? UpdatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("CreatedBy")]
+        [DataMember]
+        [Required]
+
+        public System.Guid CreatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateUpdated")]
+        [DataMember]
+
+        public DateTime? DateUpdated { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateCreated")]
+        [DataMember]
+        [Required]
+
+        public DateTime DateCreated { get; set; }
+
+    }
+}
