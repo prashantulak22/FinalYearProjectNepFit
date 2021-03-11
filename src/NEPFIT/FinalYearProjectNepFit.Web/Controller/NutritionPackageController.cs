bb98f02b0a21b7d8b92fd5dfdad40769fb5e2360
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-11 14:09:04 +0545

    added the controller section for nutritionpackage

---
+++ src/NEPFIT/FinalYearProjectNepFit.Web/Controller/NutritionPackageController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+
+namespace FinalYearProjectNepFit.Web.Controller
+{
+    [ApiController]
+    [Authorize]
+    public class NutritionPackageController : ControllerBase
+    {
+        private readonly INutritionPackageService _nutritionPackageService;
+
+        public NutritionPackageController(INutritionPackageService nutritionPackageService)
+        {
+            _nutritionPackageService = nutritionPackageService;
+        }
+
+        [Route("api/NutritionPackage/all")]
+        [HttpGet]
+        public IEnumerable<NutritionPackageResultDto> GetAll()
+        {
+            return _nutritionPackageService.GetAll();
+        }
+
+
+        [Route("api/NutritionPackage/delete")]
+        [HttpDelete]
+        public ActionResult<bool> Delete(NutritionPackageUpdateDto input)
+        {
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionPackageService.Delete(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/NutritionPackage/add")]
+        [HttpPost]
+        public ActionResult<int> Add(NutritionPackageCreateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.CreatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionPackageService.Add(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/NutritionPackage/update")]
+        [HttpPost]
+        public ActionResult<bool> Update(NutritionPackageUpdateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionPackageService.Update(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+
+}
