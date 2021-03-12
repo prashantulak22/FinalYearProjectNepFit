9b46b4d47357b1ad516fd32aad34fcc36cdc0ca3
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-12 21:13:04 +0545

    added the controller section and its content for nutrition routine

---
+++ src/NEPFIT/FinalYearProjectNepFit.Web/Controller/NutritionRoutineController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+
+namespace FinalYearProjectNepFit.Web.Controller
+{
+    [ApiController]
+    [Authorize]
+    public class NutritionRoutineController : ControllerBase
+    {
+        private readonly INutritionRoutineService _nutritionRoutineService;
+
+        public NutritionRoutineController(INutritionRoutineService nutritionRoutineService)
+        {
+            _nutritionRoutineService = nutritionRoutineService;
+        }
+
+        [Route("api/nutritionroutine/all")]
+        [HttpGet]
+        public IEnumerable<NutritionRoutineResultDto> GetAll()
+        {
+            return _nutritionRoutineService.GetAll();
+        }
+
+
+        [Route("api/nutritionroutine/delete")]
+        [HttpPost]
+        public ActionResult<bool> Delete(NutritionRoutineUpdateDto input)
+        {
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionRoutineService.Delete(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/nutritionroutine/add")]
+        [HttpPost]
+        public ActionResult<int> Add(NutritionRoutineCreateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.CreatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionRoutineService.Add(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/nutritionroutine/update")]
+        [HttpPost]
+        public ActionResult<bool> Update(NutritionRoutineUpdateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _nutritionRoutineService.Update(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+
+}
