eecbdeb82c9e41b8ce8fbc73d0d29e4e950421f4
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-12 20:54:29 +0545

    Added the interface layer for nutritionroutineservice

---
+++ src/NEPFIT/NepFit.BL/Interface/INutritionRoutineService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NepFit.Repository.Dto;
+
+namespace NepFit.BL.Interface
+{
+    public interface INutritionRoutineService
+    {
+        int Add(NutritionRoutineCreateDto input);
+        bool Update(NutritionRoutineUpdateDto input);
+        IEnumerable<NutritionRoutineResultDto> GetAll();
+        bool Delete(NutritionRoutineUpdateDto id);
+    }
+}
