0deca9d0a15fd7d927b382bc20c722aefbe3a995
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-12 20:25:05 +0545

    added the create, update and delete dto for nutirtionroutine

---
+++ src/NEPFIT/NepFit.Repository/Dto/NutritionRoutineCreateDto.cs
@@ -0,0 +1,92 @@
+ 
+using SalarDb.CodeGen.Entities;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+namespace SalarDb.CodeGen.DTO
+{
+    public class NutritionRoutineCreateDto 
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Name")]
+        [DataMember]
+        [Required]
+        [StringLength(5000)]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Description")]
+        [DataMember]
+        [Required]
+        [StringLength(5000)]
+        public String Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("HowToPrepare")]
+        [DataMember]
+        [Required]
+        [StringLength(5000)]
+        public String HowToPrepare { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Active")]
+        [DataMember]
+        
+        public Boolean? Active { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("UpdatedBy")]
+        [DataMember]
+        
+        public System.Guid? UpdatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("CreatedBy")]
+        [DataMember]
+        [Required]
+        
+        public System.Guid CreatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateUpdated")]
+        [DataMember]
+        
+        public DateTime? DateUpdated { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateCreated")]
+        [DataMember]
+        [Required]
+        
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("NutritionTypeId")]
+        [DataMember]
+        [Required]
+        
+        public System.Guid NutritionTypeId { get; set; }
+
+
+    }
+}
