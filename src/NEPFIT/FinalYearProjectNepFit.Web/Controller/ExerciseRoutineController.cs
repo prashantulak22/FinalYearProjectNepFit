89177c4ae62a49efe4605ca4d88d256a95b1b418
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-12 15:44:57 +0545

    added the controller section for exerciseroutine

--- src/NEPFIT/FinalYearProjectNepFit.Web/Controller/ExerciseRoutineController.cs
+++ src/NEPFIT/FinalYearProjectNepFit.Web/Controller/ExerciseRoutineController.cs
@@ -10,17 +10,69 @@ namespace FinalYearProjectNepFit.Web.Controller
 {
     [ApiController]
     [Authorize]
-
     public class ExerciseRoutineController : ControllerBase
     {
         private readonly IExerciseRoutineService _exerciseRoutineService;
 
-        public ExerciseRoutineController(IExerciseRoutineService exerciseRoutineService)
+        public ExerciseRoutineController(IExerciseRoutineService ExerciseRoutineService)
         {
-            _exerciseRoutineService = exerciseRoutineService;
+            _exerciseRoutineService = ExerciseRoutineService;
         }
 
-        //[Route("api/exercise/routine")]
-       //[HttpPost]
+        [Route("api/exerciseroutine/all")]
+        [HttpGet]
+        public IEnumerable<ExerciseRoutineResultDto> GetAll()
+        {
+            return _exerciseRoutineService.GetAll();
+        }
+
+
+        [Route("api/exerciseroutine/delete")]
+        [HttpDelete]
+        public ActionResult<bool> Delete(ExerciseRoutineUpdateDto input)
+        {
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _exerciseRoutineService.Delete(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/exerciseroutine/add")]
+        [HttpPost]
+        public ActionResult<int> Add(ExerciseRoutineCreateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.CreatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _exerciseRoutineService.Add(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [Route("api/exerciseroutine/update")]
+        [HttpPost]
+        public ActionResult<bool> Update(ExerciseRoutineUpdateDto input)
+        {
+
+            if (TryValidateModel(input))
+            {
+                input.UpdatedBy = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                return _exerciseRoutineService.Update(input);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
     }
+
 }
