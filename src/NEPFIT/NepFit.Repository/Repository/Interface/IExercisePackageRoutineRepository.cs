23647c6c31dc2b7df596db42d392ae07334652a8
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-13 16:22:51 +0545

    added the interface layer for exercisepackageroutine for repo section

--- src/NEPFIT/NepFit.Repository/Repository/Interface/IExercisePackageRoutineRepository.cs
+++ src/NEPFIT/NepFit.Repository/Repository/Interface/IExercisePackageRoutineRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using NepFit.Repository.Entity;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace NepFit.Repository.Repository.Interface
 {
-    class IExercisePackageRoutineRepository
+    public interface IExercisePackageRoutineRepository
     {
+        int Add(ExercisePackageRoutine input);
+        ExercisePackageRoutine Update(ExercisePackageRoutine input);
+        IEnumerable<ExercisePackageRoutine> GetAll();
+        ExercisePackageRoutine GetById(Guid id);
+        bool Delete(ExercisePackageRoutine input);
     }
 }
