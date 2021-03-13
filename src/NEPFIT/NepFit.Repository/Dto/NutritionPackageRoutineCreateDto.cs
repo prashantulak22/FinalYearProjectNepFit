6e50327aa94878c08084b5ef64ef74a56eca4fd1
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-13 16:02:01 +0545

    added the dto file for nutirtionpackageroutine

---
+++ src/NEPFIT/NepFit.Repository/Dto/NutritionPackageRoutineCreateDto.cs
@@ -0,0 +1,72 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+namespace NepFit.Repository.Dto
+{
+    public class NutritionPackageRoutineCreateDto 
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("NutritionPackageId")]
+        [DataMember]
+        [Required]
+        
+        public System.Guid NutritionPackageId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("NutritionRoutineId")]
+        [DataMember]
+        [Required]
+        
+        public System.Guid NutritionRoutineId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("Active")]
+        [DataMember]
+        
+        public Boolean? Active { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("UpdatedBy")]
+        [DataMember]
+        
+        public System.Guid? UpdatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("CreatedBy")]
+        [DataMember]
+        [Required]
+        
+        public System.Guid CreatedBy { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateUpdated")]
+        [DataMember]
+        
+        public DateTime? DateUpdated { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DisplayName("DateCreated")]
+        [DataMember]
+        [Required]
+        
+        public DateTime DateCreated { get; set; }
+
+
+    }
+}
