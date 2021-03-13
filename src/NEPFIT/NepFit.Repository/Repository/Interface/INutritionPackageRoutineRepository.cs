69377fce055a743bcd29e199bfd61ef277f47752
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-13 16:08:50 +0545

    added the interface layer for nutritionpackageroutinr repo

---
+++ src/NEPFIT/NepFit.Repository/Repository/Interface/INutritionPackageRoutineRepository.cs
@@ -0,0 +1,15 @@
+﻿using NepFit.Repository.Dto;
+using System;
+using System.Collections.Generic;
+
+namespace NepFit.Repository.Repository.Interface
+{
+    public interface INutritionPackageRoutineRepository
+    {
+        int Add(NutritionPackageRoutine input);
+        NutritionPackageRoutine Update(NutritionPackageRoutine input);
+        IEnumerable<NutritionPackageRoutine> GetAll();
+        NutritionPackageRoutine GetById(Guid id);
+        bool Delete(NutritionPackageRoutine input);
+    }
+}
