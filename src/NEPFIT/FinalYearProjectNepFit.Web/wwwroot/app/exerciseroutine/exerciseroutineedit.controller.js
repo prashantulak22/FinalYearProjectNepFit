2762513afbc73a06db250d148076d546f012575e
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-27 17:07:06 -0500

    fix for bind on edit not working

--- src/NEPFIT/FinalYearProjectNepFit.Web/wwwroot/app/exerciseroutine/exerciseroutineedit.controller.js
+++ src/NEPFIT/FinalYearProjectNepFit.Web/wwwroot/app/exerciseroutine/exerciseroutineedit.controller.js
@@ -19,12 +19,14 @@
             blockUI.stop();
         }
         function activate() {
+            vm.exerciseRoutine = param.item;
             exerciseTypeService.getAllExerciseType()
                 .then(function (result) {
                     vm.exerciseTypeIdDropdownOptions = {
                         dataTextField: "name",
                         dataValueField: "exerciseTypeId",
                         valuePrimitive: true,
+                        value: vm.exerciseRoutine.exerciseTypeId,
                         dataSource: {
 
                             data: result.data
@@ -36,43 +38,43 @@
 
                 });
 
-            
 
-               
+
+
+            vm.validationError = [];
+
+            function validateForm() {
                 vm.validationError = [];
-
-                function validateForm() {
-                    vm.validationError = [];
-                    if (vm.validationError.length > 0) return false;
-                    if ($scope.validator.validate()) return true;
-                    return false;
-                }
-
-                vm.saveExerciseRoutine = function (option) {
-                    if (!validateForm()) return;
-                    showLoading();
-                    exerciseRoutineService.updateExerciseRoutine(vm.exerciseRoutine)
-                        .then(function () {
-                            hideLoading();
-                            vm.close();
-                        }, function errorCallback(response) {
-                            hideLoading();
-                        });
-                }
-
-                vm.close = function () {
-                    $uibModalInstance.close();
-                };
-
-
-
+                if (vm.validationError.length > 0) return false;
+                if ($scope.validator.validate()) return true;
+                return false;
             }
+
+            vm.saveExerciseRoutine = function (option) {
+                if (!validateForm()) return;
+                showLoading();
+                exerciseRoutineService.updateExerciseRoutine(vm.exerciseRoutine)
+                    .then(function () {
+                        hideLoading();
+                        vm.close();
+                    }, function errorCallback(response) {
+                        hideLoading();
+                    });
+            }
+
+            vm.close = function () {
+                $uibModalInstance.close();
+            };
+
 
 
         }
 
 
-    
+    }
+
+
+
 
 
 })(angular, $, kendo);
