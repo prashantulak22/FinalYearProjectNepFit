8f62e77fa588518bebef2cd4f2d14f6a3a1eb298
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-27 20:07:40 +0545

    added a primary key to the userexercisenutrition table

--- src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
+++ src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[UserExerciseNutrition]
 (
+	[UserExerciseNutritionId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
 	Active bit,
 	UpdatedBy UNIQUEIDENTIFIER NULL,
 	CreatedBy UNIQUEIDENTIFIER NOT NULL,
 	DateUpdated DateTime NULL,
 	DateCreated DateTime NOT NULL,
 
-	[UserId] UNIQUEIDENTIFIER PRIMARY KEY NOT NULL,
+	[UserId] UNIQUEIDENTIFIER NOT NULL,
 	CONSTRAINT [FK_UserId]FOREIGN KEY ([UserId]) REFERENCES [NepFitUser]([UserId]),
 	[ExerciseNutritionPackageId] UNIQUEIDENTIFIER NOT NUll,
     CONSTRAINT [FK_User_ExerciseNutrition] FOREIGN KEY ([ExerciseNutritionPackageId]) REFERENCES [ExerciseNutritionPackage]([ExerciseNutritionPackageId]), 
