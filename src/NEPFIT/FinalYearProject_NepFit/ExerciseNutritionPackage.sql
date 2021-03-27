887d12a7bd21b4edbcb660caf1af9f886af401e7
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-27 12:44:18 +0545

    Added new tables and colums in them

---
+++ src/NEPFIT/FinalYearProject_NepFit/ExerciseNutritionPackage.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ExerciseNutritionPackage]
+(
+	[ExerciseNutritionPackageId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
+	[ExercisePackageId] UNIQUEIDENTIFIER NOT NULL,
+	[NutritionPackageId] UNIQUEIDENTIFIER NOT NULL,
+	Active bit,
+	UpdatedBy UNIQUEIDENTIFIER NULL,
+	CreatedBy UNIQUEIDENTIFIER NOT NULL,
+	DateUpdated DateTime NULL,
+	DateCreated DateTime NOT NULL
+	CONSTRAINT [FK_ExercisePackage] FOREIGN KEY ([ExercisePackageId]) REFERENCES [ExercisePackage]([ExercisePackageId])
+	CONSTRAINT [FK_NutriitonPackage] FOREIGN KEY ([NutritionPackageId]) REFERENCES [NutritionPackage]([NutritionPackageId])
+)
--- src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
+++ src/NEPFIT/FinalYearProject_NepFit/UserExerciseNutrition.sql
@@ -1,10 +1,15 @@
 ﻿CREATE TABLE [dbo].[UserExerciseNutrition]
 (
+	Active bit,
+	UpdatedBy UNIQUEIDENTIFIER NULL,
+	CreatedBy UNIQUEIDENTIFIER NOT NULL,
+	DateUpdated DateTime NULL,
+	DateCreated DateTime NOT NULL,
+
 	[UserId] UNIQUEIDENTIFIER PRIMARY KEY NOT NULL,
 	CONSTRAINT [FK_UserId]FOREIGN KEY ([UserId]) REFERENCES [NepFitUser]([UserId]),
-	[ExercisePackageId] UNIQUEIDENTIFIER NOT NUll,
-    CONSTRAINT [FK_User_Exercise] FOREIGN KEY ([ExercisePackageId]) REFERENCES [ExercisePackage]([ExercisePackageId]), 
-	[NutritionTypeId] UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [FK_User_NutritionType] FOREIGN KEY ([NutritionTypeId]) REFERENCES [NutritionType]([NutritionTypeId]) 
+	[ExerciseNutritionPackageId] UNIQUEIDENTIFIER NOT NUll,
+    CONSTRAINT [FK_User_ExerciseNutrition] FOREIGN KEY ([ExerciseNutritionPackageId]) REFERENCES [ExerciseNutritionPackage]([ExerciseNutritionPackageId]), 
+	 
 	
 )
