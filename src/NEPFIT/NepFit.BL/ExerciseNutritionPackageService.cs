7466c1d0ec3b3e6b8be8f7b81c1aa286167a8a64
Prashant Ulak <np01cp4a180016@islingtoncollege.edu.np>
2021-03-27 18:39:19 +0545

    added exercisenutirtionpackage service

---
+++ src/NEPFIT/NepFit.BL/ExerciseNutritionPackageService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using NepFit.BL.Interface;
+using NepFit.Repository.Dto;
+using NepFit.Repository.Entity;
+using NepFit.Repository.Repository.Interface;
+
+namespace NepFit.BL
+{
+    public class ExerciseNutritionPackageService : IExerciseNutritionPackageService
+    {
+        private readonly IExerciseNutritionPackageRepository _exerciseNutritionPackageRepository;
+        private readonly IMapper _mapper;
+
+        public ExerciseNutritionPackageService(IExerciseNutritionPackageRepository ExerciseNutritionPackageRepository, IMapper mapper)
+        {
+            _exerciseNutritionPackageRepository = ExerciseNutritionPackageRepository;
+            _mapper = mapper;
+        }
+
+        public int Add(ExerciseNutritionPackageCreateDto inputDto)
+        {
+            inputDto.DateCreated = DateTime.Now;
+            inputDto.DateUpdated = DateTime.Now;
+            inputDto.UpdatedBy = inputDto.CreatedBy;
+            inputDto.Active = true;
+            return _exerciseNutritionPackageRepository.Add(_mapper.Map<ExerciseNutritionPackage>(inputDto));
+        }
+
+        public bool Update(ExerciseNutritionPackageUpdateDto input)
+        {
+            var original = _exerciseNutritionPackageRepository.GetById(input.ExerciseNutritionPackageId);
+            original.DateUpdated = DateTime.Now;
+            _mapper.Map(input, original);
+            _exerciseNutritionPackageRepository.Update(original);
+            return true;
+        }
+
+        public IEnumerable<ExerciseNutritionPackageResultDto> GetAll()
+        {
+            return
+                _mapper.Map<IEnumerable<ExerciseNutritionPackageResultDto>>(
+                _exerciseNutritionPackageRepository.GetAll());
+        }
+
+        public bool Delete(ExerciseNutritionPackageUpdateDto input)
+        {
+            var deleteObj = _mapper.Map<ExerciseNutritionPackage>(input);
+            deleteObj.DateUpdated = DateTime.Now;
+            return _exerciseNutritionPackageRepository.Delete(deleteObj);
+        }
+    }
+}
